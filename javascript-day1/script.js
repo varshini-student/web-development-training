f9e4d610342b4c60ab027222290eab1fc7822919
varshini-student <mvarshini1708@gmail.com>
2026-02-02 16:17:37 +0530

    JavaScript Day 1: Variables, conditions and functions

---
+++ javascript-day1/script.js
@@ -0,0 +1,14 @@
+// variable initialization
+let name = "Varshini";
+let age = 20;
+// if-else condition
+if (age > 18) {
+    console.log("Eligible");
+}
+// function
+function greet() {
+    console.log("Hello " + name);
+}
+
+greet();
+
