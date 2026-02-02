9991b63d73a5e4d7eeef8cb16970f8f960a51aa2
varshini-student <mvarshini1708@gmail.com>
2026-02-02 16:27:20 +0530

    JavaScript Day 2: DOM manipulation basics

---
+++ javascript-day2/script.js
@@ -0,0 +1,7 @@
+let heading = document.getElementById("title");
+
+function changeText() {
+    heading.innerText = "Text Changed using JavaScript!";
+    heading.style.color = "blue";
+    heading.style.fontSize = "32px";
+}
