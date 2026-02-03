c27642a51963014784b5259744e106328e990654
varshini-student <mvarshini1708@gmail.com>
2026-02-03 15:30:58 +0530

    JavaScript Day 4: map, filter, reduce and JSON basics

---
+++ javascript-day4/script.js
@@ -0,0 +1,15 @@
+// map
+let nums = [1, 2, 3];
+console.log(nums.map(n => n + 1));
+
+// filter
+let marks = [35, 60, 80];
+console.log(marks.filter(m => m >= 50));
+
+// reduce
+let bills = [100, 200, 300];
+console.log(bills.reduce((t, b) => t + b, 0));
+
+// JSON object
+let student = { name: "Varshini", age: 20 };
+console.log(student.name);
