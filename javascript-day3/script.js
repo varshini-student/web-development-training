0a3a0ab02a1aa39b7d56544f4951e866c55e4389
varshini-student <mvarshini1708@gmail.com>
2026-02-03 11:00:17 +0530

    Day 3: Events,arrays,and object practice

---
+++ javascript-day3/script.js
@@ -0,0 +1,26 @@
+// Event
+function buttonClicked(){
+    console.log("Button was clicked");
+}
+buttonClicked();
+
+// Array
+let fruits = ["apple","mango","grapes"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+// loop through array
+for(let i=0;i<fruits.length;i++){
+    console.log(arr[i]);
+}
+
+// Objects
+let student ={
+    name:"Sugu",
+    age:22,
+    subject:"SPM"
+};
+console.log(student.name);
+console.log(student.age);
+console.log(student.subject);
